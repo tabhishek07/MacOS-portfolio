889c6fa0816c9e0221e22864ced26f1273d301a9
Abhishek Tiwari <163037775+tabhishek07@users.noreply.github.com>
2025-11-25 18:33:53 +0530

    first commit having setup nad landing page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold"> Abhishek's Portfolio </p>
+
+                <ul>
+                    {navLinks.map(( { id, name }) =>(
+                        <li key = {id}>
+                            <p> {name} </p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    { navIcons.map( ( { id, img }) => (
+                    <li key={id} >
+                       <img src= {img} className="icon-hover"  alt={ `icon-${id}` }/> 
+                    </li>
+                ))}
+                </ul>
+                <time> { dayjs().format('ddd MMM D h:mm A') } </time>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
